c38c05f487ba2103ecc93910b74dd8e418ff7224
Siddique-Ahmad <Siddique@inzerotech.com>
2022-04-28 11:29:54 +0530

    fix for Barcode_28th April

--- ProteusMMX_382/ProteusMMX/ProteusMMX/AppSettings.cs
+++ ProteusMMX_382/ProteusMMX/ProteusMMX/AppSettings.cs
@@ -28,7 +28,7 @@ namespace ProteusMMX
 
         public static string APPVersion
         {
-            get => Settings.GetValueOrDefault(nameof(APPVersion), "3.14.7");
+            get => Settings.GetValueOrDefault(nameof(APPVersion), "3.14.10");
         }
         public static string UserName
         {
--- ProteusMMX_382/ProteusMMX/ProteusMMX/ViewModel/Workorder/AddNewToolViewModel.cs
+++ ProteusMMX_382/ProteusMMX/ProteusMMX/ViewModel/Workorder/AddNewToolViewModel.cs
@@ -861,12 +861,15 @@ namespace ProteusMMX.ViewModel.Workorder
 
                     };
 
+                    options.PossibleFormats = new List<ZXing.BarcodeFormat>() { ZXing.BarcodeFormat.CODE_39, ZXing.BarcodeFormat.CODE_93, ZXing.BarcodeFormat.CODE_128, ZXing.BarcodeFormat.EAN_13, ZXing.BarcodeFormat.QR_CODE };
+                    options.TryHarder = false; options.BuildBarcodeReader().Options.AllowedLengths = new[] { 44 };
                     ZXingScannerPage _scanner = new ZXingScannerPage(options)
                     {
                         DefaultOverlayTopText = "Align the barcode within the frame",
                         DefaultOverlayBottomText = string.Empty,
                         DefaultOverlayShowFlashButton = true
                     };
+                    _scanner.AutoFocus();
 
                     _scanner.OnScanResult += _scanner_OnScanResult;
                     var navPage = App.Current.MainPage as NavigationPage;
